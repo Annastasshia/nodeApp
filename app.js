8806464fb8b5f5b0a7bd97bc2df3530b9ee7a7ce
Annastasshia <aames26@gmail.com>
2021-01-05 12:40:08 -0800

    add node modules OS HTTP Emitter

--- app.js
+++ app.js
@@ -1,8 +1,71 @@
-var logger = require('./logger');
+// const Logger = require('./logger');
+// const logger = new Logger();
 
-function sayHello(name) {
-    console.log('Hello ' + name);
-}
 
-sayHello('Annastasshia')
-logger.log('blah');
+// function sayHello(name) {
+//     console.logger('Hello ' + name);
+// }
+
+// sayHello('Annastasshia')
+// log('blah-blah');
+
+
+//os node module usage example
+
+const os =require('os');
+
+var totalMem = os.totalmem();
+var freeMem = os.freemem();
+
+console.log('Total Memory: ' + totalMem)
+console.log('Total Memory: ' + freeMem)
+
+
+//file system node module usage example
+
+const fs = require('fs');
+
+    //sync
+    const files = fs.readdirSync('./');
+    console.log(files);
+
+    //async (prefered)
+    fs.readdir('./', function(err, files){
+        if(err) console.log('error', err);
+        else console.log('Result', files);
+    })
+
+
+//events node module usage example (rare to work with it directly)
+
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+
+    //register a listener for events
+    emitter.on('messageLogged', function(arg){
+        console.log('Listener Called', arg);
+    })
+
+    //raise an event with an argument
+    emitter.emit('messageLogged', {id: 1, url:'http://'});
+
+
+// HTTP node module usage example ( low level server request and responses )
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('Hello World');
+        res.end();
+    }
+
+    if (req.url === '/api/courses') {
+        res.write(JSON.stringify([1, 2, 3]));
+        res.end();
+    }
+});
+
+server.listen(3000);
+
+console.log('listening on port 3000 ....')
--- logger.js
+++ logger.js
@@ -1,7 +1,17 @@
+
+const EventEmitter = require('events');
+
 var url = 'http://mylogger.io/log';
 
-function log(message) {
-    console.log(message);
+class Logger extends EventEmitter{
+    log(message) {
+        console.log(message);
+    
+        //raise an event with an argument
+        this.emit('messageLogged', {id: 1, url:'http://'});
+    }
+
 }
 
-module.exports.log = log;
+
+module.exports = Logger;
