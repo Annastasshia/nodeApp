c0c8916e7331de9809295fabd6aa4f51842ef877
Annastasshia <aames26@gmail.com>
2021-01-07 09:48:07 -0800

    GET requests add/edit

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-var _ = require('underscore');
+const _ = require('underscore');
 const express = require('express');
 const app = express();
+
+const courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' },
+]
 
 //endpoints
 app.get('/', (req, res) => {
     res.send('Hello World!!');
-})
+});
 
 app.get('/api/courses', (req, res) => {
-    res.send([class1,class2,class3]);
-})
+    res.send(courses);
+});
 
-app.get('/api/courses/:id',(req,res)=> {
-    res.send(req.params.id)
-})
+app.get('/api/courses/:id', (req,res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with that given ID was not found')
+    res.send(course);
+});
 
 app.get('/api/posts/:year/:month',(req,res) => {
     res.send(req.params)
-})
+});
 
 //Litening port | Enviroment variable
 
