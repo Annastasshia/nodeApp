b1981fb8a336f723da475611ad51664c954383d4
Annastasshia <aames26@gmail.com>
2021-01-07 09:22:10 -0800

    nodemon and set enviro variable

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ var _ = require('underscore');
 const express = require('express');
 const app = express();
 
+//endpoints
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello World!!');
 })
 
 app.get('/api/courses', (req, res) => {
     res.send([1,2,3]);
 })
 
-app.listen(3000, () => console.log('listening on port 3000....'))
+//Litening port | Enviroment variable
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log('listening on port ' + port + '....'))
 
 //searches for a num in a provided array and outputs a boolan
 var result= _.contains([1, 2, 3], 4)
