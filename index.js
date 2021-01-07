f7219c817e7046f16495039032d44319bd550ad4
Annastasshia <aames26@gmail.com>
2021-01-07 09:07:30 -0800

    add express framework

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var _ = require('underscore');
+const express = require('express');
+const app = express();
 
+app.get('/', (req, res) => {
+    
+})
+
+//searches for a num in a provided array and outputs a boolan
 var result= _.contains([1, 2, 3], 4)
 
 console.log(result);
