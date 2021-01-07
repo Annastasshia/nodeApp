8eb823858ed61d133e13870c821734e543002470
Annastasshia <aames26@gmail.com>
2021-01-07 10:30:50 -0800

    add POST requests & Validation

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
+//dependencies
 const _ = require('underscore');
 const express = require('express');
 const app = express();
+const Joi = require('joi');
 
+//middleware
+app.use(express.json());
+
+//data array
 const courses = [
     { id: 1, name: 'course1' },
     { id: 2, name: 'course2' },
     { id: 3, name: 'course3' },
 ]
 
-//endpoints
+//GET endpoints
 app.get('/', (req, res) => {
     res.send('Hello World!!');
 });
@@ -26,6 +32,34 @@ app.get('/api/courses/:id', (req,res) => {
 app.get('/api/posts/:year/:month',(req,res) => {
     res.send(req.params)
 });
+
+//POST requests | test with postman
+app.post('/api/courses', (req,res) => {
+
+    //validation logic without joi : set Min characters 
+    if(!req.body.name || req.body.name.length < 3) {
+        //400 Bad Request
+        res.status(400).send('Name needs at least 3 characters');
+        return;
+    };
+
+    // //validation logic with Joi : set Max characters
+        // const schema = {
+        //     name: Joi.string().max(7).required
+        // };
+
+        // const result = Joi.validate(req.body, schema);
+        // console.log (result);
+
+    const course ={
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+})
+
+//PUT requests
 
 //Litening port | Enviroment variable
 
