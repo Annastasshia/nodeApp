def53bc05a860e133d6bfd3df0f0d1bfdddfd4ff
Annastasshia <aames26@gmail.com>
2021-01-07 09:11:20 -0800

    add initial endpoints

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    
+    res.send('Hello World');
 })
+
+app.get('/api/courses', (req, res) => {
+    res.send([1,2,3]);
+})
+
+app.listen(3000, () => console.log('listening on port 3000....'))
 
 //searches for a num in a provided array and outputs a boolan
 var result= _.contains([1, 2, 3], 4)
